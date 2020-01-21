1553c3cc4e4e4e47570a02d3a5aede06e1a74633
MaorMin <maorminyan@gmail.com>
2020-01-21 18:13:39 +0200

    Intent Update

--- app/src/main/java/com/example/gymproject/MainActivity.java
+++ app/src/main/java/com/example/gymproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gymproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button regBtn = findViewById(R.id.reg_btn);
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,registerPage.class);
+                startActivity(intent);
+            }
+        });
+        Button loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,DetailsPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
