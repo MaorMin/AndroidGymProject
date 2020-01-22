49f448497a50571ab9283f7193a24609d3a01466
MaorMin <maorminyan@gmail.com>
2020-01-23 01:08:13 +0200

    added firebase

--- app/src/main/java/com/example/gymproject/DataBaseTest.java
+++ app/src/main/java/com/example/gymproject/DataBaseTest.java
@@ -1,4 +1,6 @@
 package com.example.gymproject;
 
 public class DataBaseTest {
+
+
 }
--- app/src/main/java/com/example/gymproject/MainActivity.java
+++ app/src/main/java/com/example/gymproject/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.gymproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +23,13 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -39,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private CallbackManager callbackManager;
+    private static String TAG = "Facebook login";
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -46,33 +57,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //--------Firebase login---//
+        // Initialize Facebook Login button
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
-
-        loginButton = findViewById(R.id.loginFbBtn);
-        //circleImageView = findViewById(R.id.profile_picFb);
         callbackManager = CallbackManager.Factory.create();
-
-        //txtEmail = findViewById(R.id.profile_email);
-       // txtName = findViewById(R.id.profile_name);
-
-        loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
-
+        loginButton = findViewById(R.id.loginFbBtn);
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
             public void onCancel() {
-
+                Log.d(TAG, "facebook:onCancel");
+                // ...
             }
 
             @Override
             public void onError(FacebookException error) {
-
+                Log.d(TAG, "facebook:onError", error);
+                // ...
             }
         });
+// ...
+
+
+//---fb
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+
+        //circleImageView = findViewById(R.id.profile_picFb);
+
+
+        //txtEmail = findViewById(R.id.profile_email);
+        // txtName = findViewById(R.id.profile_name);
+
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
+
 
         Button regBtn = findViewById(R.id.reg_btn);
         regBtn.setOnClickListener(new View.OnClickListener() {
@@ -89,6 +115,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, DetailsPage.class);
                 startActivity(intent);
+
+
             }
         });
 
@@ -106,9 +134,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode != 0) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != 0) {
             Intent intent = new Intent(MainActivity.this, DetailsPage.class);
             startActivity(intent);
         }
@@ -157,4 +186,55 @@ public class MainActivity extends AppCompatActivity {
         request.executeAsync();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser;
+        currentUser = mAuth.getCurrentUser();
+
+
+        if (currentUser != null) {
+            updateUI();
+        }
+    }
+    private void handleFacebookAccessToken (AccessToken token){
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    private void updateUI () {
+        Toast.makeText(MainActivity.this, "Congratulations, you are logged in", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(MainActivity.this, DataBaseTest.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+
 }
+
+
+
