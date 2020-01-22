4046d4d371b2acfe36518551c7bb940d6b11e460
haimp2D <haimp2@gmail.com>
2020-01-22 21:12:40 +0200

    Fixed Maor's mess

---
+++ app/src/main/java/com/example/gymproject/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.gymproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        int SPLASH_SCREEN_TIME_OUT = 1500;
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
